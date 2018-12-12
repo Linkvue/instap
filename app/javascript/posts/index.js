62524e5e03f36b2373e740d481972af909f3d8bd
Qinglong Zhang <zhang.qinglong@hylinkad.com>
2018-12-12 16:31:05 +0800

    Add CKEditor

--- app/javascript/posts/index.js
+++ app/javascript/posts/index.js
@@ -1,34 +1,10 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InstapEditor from './components/instap_editor';
-import Editor from './components/medium_editor';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = document.querySelector('#root');
+import Editor from './components/ck_editor'
+
+
 ReactDOM.render(
   <Editor />,
-  root
+  document.querySelector('.editable')
 )
-*/
-
-import MediumEditor from 'medium-editor';
-
-import css from './styles/index.css'
-
-var editor = new MediumEditor('.editable', {
-    toolbar: {
-        allowMultiParagraphSelection: true,
-        buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote'],
-        diffLeft: 0,
-        diffTop: -10,
-        firstButtonClass: 'medium-editor-button-first',
-        lastButtonClass: 'medium-editor-button-last',
-        relativeContainer: null,
-        standardizeSelectionStart: false,
-        static: false,
-        /* options which only apply when static is true */
-        align: 'center',
-        sticky: false,
-        updateOnEmptySelection: false
-    }
-});
